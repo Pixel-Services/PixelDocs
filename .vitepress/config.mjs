ba4d97f241a26e2dee000cb3b7e26bcf65a820b3
Relism <itzdrekkopvp@gmail.com>
2024-12-15 04:01:43 +0100

    Updated site metadata.

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -1,9 +1,12 @@
 import sidebarConfig from '../composer.js'
 
 export default {
+    title: 'Pixel Services Docs',
+    description: 'Documentation for all public Pixel Services projects.',
+    siteTitle: 'Documentation for all public Pixel Services projects.',
+    head: [['link', { rel: 'icon', href: 'https://static.pixel-services.com/static/assets/pservices_logo.png' }]],
     themeConfig: {
-        siteTitle: 'Pixel Services Docs',
         logo: 'https://static.pixel-services.com/static/assets/pservices_logo.png',
-        sidebar: sidebarConfig
+        sidebar: sidebarConfig,
     }
 }
