3e8b7aa088d9a278f370bc5e0585f2786e7db787
Relism <Relism@users.noreply.github.com>
2024-12-16 18:34:57 +0000

    deploy: 1d908ae2eb4543168c28e689618df8178b31a403

---
+++ assets/mobot_index.md.BospPqKT.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o}from"./chunks/framework.BGabeMLJ.js";const _=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"mobot/index.md","filePath":"mobot/index.md"}'),a={name:"mobot/index.md"};function n(r,s,c,d,i,m){return o(),t("div")}const f=e(a,[["render",n]]);export{_ as __pageData,f as default};
